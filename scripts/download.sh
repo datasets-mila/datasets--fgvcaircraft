5314eb1006cc0dd99be88c5dad328a5ffa74756c
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-11-03 15:59:26 -0500

    Add download script

---
+++ scripts/download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source scripts/utils.sh echo -n
+
+# Saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail
+
+# This script is meant to be used with the command 'datalad run'
+
+files_url=(
+	"https://www.robots.ox.ac.uk/~vgg/data/fgvc-aircraft/archives/fgvc-aircraft-2013b.tar.gz fgvc-aircraft-2013b.tar.gz")
+
+# These urls require login cookies to download the file
+git-annex addurl --fast -c annex.largefiles=anything --raw --batch --with-files <<EOF
+$(for file_url in "${files_url[@]}" ; do echo "${file_url}" ; done)
+EOF
+git-annex get --fast -J8
+git-annex migrate --fast -c annex.largefiles=anything *
+
+[[ -f md5sums ]] && md5sum -c md5sums
+[[ -f md5sums ]] || md5sum $(git-annex list --fast | grep -o " .*") > md5sums
